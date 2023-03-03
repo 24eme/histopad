261b548ea9f720a21b9f7526ffa269cbbe67123b
Vincent LAURENT <vince.laurent@gmail.com>
2023-03-03 10:48:20 +0100

    Modal viewer tableau dans le contenu

--- viewer.php
+++ viewer.php
@@ -7,28 +7,28 @@ $pad = PadClient::find($_GET['file']);
 
 ?>
 <div class="modal-header">
-    <h4 class="modal-title">
+    <h5 class="modal-title">
         <?php echo $pad->title; ?>
-    </h4>
+    </h5>
 
     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
 </div>
-<table class="table table-striped table-bordered table-sm mb-0">
-    <tr>
-        <th class="col-3">Dernière modification</th>
-        <td><?php echo $pad->date->format('d/m/Y à H\hi'); ?></td>
-    </tr>
-    <tr>
-        <th>Archivage planifié</th>
-        <td><?php if($pad->getDateNextArchivage()): ?><?php echo $pad->getDateNextArchivage()->format('d/m/Y à H\hi'); ?><?php else : ?>Aucun<?php endif; ?></td>
-    </tr>
-    <tr>
-        <th>Export</th>
-        <td><a href="<?php echo $pad->uri.'.txt'; ?>">Texte</a> | <a href="<?php echo $pad->uri.'.md'; ?>">Markdown</a> | <a href="<?php echo $pad->uri.'.html'; ?>">HTML</a> | <a href="<?php echo $pad->uri.'.etherpad'; ?>">Etherpad</a> | <a href="<?php echo $pad->url; ?>">Lien</a></td>
-    </tr>
-</table>
 <div class="modal-body">
+    <table class="table table-striped table-bordered table-sm">
+        <tr>
+            <th class="col-3">Dernière modification</th>
+            <td><?php echo $pad->date->format('d/m/Y à H\hi'); ?></td>
+        </tr>
+        <tr>
+            <th>Archivage planifié</th>
+            <td><?php if($pad->getDateNextArchivage()): ?><?php echo $pad->getDateNextArchivage()->format('d/m/Y à H\hi'); ?><?php else : ?>Aucun<?php endif; ?></td>
+        </tr>
+        <tr>
+            <th>Export</th>
+            <td><a href="<?php echo $pad->uri.'.txt'; ?>">Texte</a> | <a href="<?php echo $pad->uri.'.md'; ?>">Markdown</a> | <a href="<?php echo $pad->uri.'.html'; ?>">HTML</a> | <a href="<?php echo $pad->uri.'.etherpad'; ?>">Etherpad</a> | <a href="<?php echo $pad->url; ?>">Lien</a></td>
+        </tr>
+    </table>
     <?php echo $pad->getContent(); ?>
 </div>
