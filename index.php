0b5cd429c29ad8fe2a191bc9285f435611447502
Vincent LAURENT <vince.laurent@gmail.com>
2024-10-21 16:16:31 +0200

    Logo et favicon

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if(isset($_GET['id'])) {
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="icon" href="favicon.ico" />
     <link rel="stylesheet" href="vendor/bootstrap.min.css">
 </head>
 <body>
